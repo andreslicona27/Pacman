e6a53cd9565de82a7c346c568163553e412a61c8
Andres Licona <andreslicona27@icloud.com>
2023-03-20 21:24:32 +0100

    project creation

---
+++ Pacman/Pacman/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pacman
+{
+    public partial class Form1 : Form
+    {
+        bool group;
+        bool goUp;
+        bool goDown;
+        bool goRigth;
+        bool goLeft;
+        bool gameOver;
+        int score;
+        int playerSpeed;
+        int redGhostSpeed;
+        int yellowGhostSpeed;
+        int pinkGhost1;
+        int pinkGhost2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainGameTimer(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+    }
+}
